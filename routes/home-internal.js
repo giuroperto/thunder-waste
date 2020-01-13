2d692925478a48e54640677e4906a033f14dfbcd
Giu Roperto <giu.roperto@gmail.com>
2020-01-13 14:55:34 -0300

    adjusting routes

--- routes/home-internal.js
+++ routes/home-internal.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-router.get(('/staff/home'), (req, res, next) => {
+router.get(('/home'), (req, res, next) => {
   res.render('internal-home');
 });
 
-router.get('/staff/pro')
+// router.get('/all', (req, res, next) => {
+//   User.find({accountType: 'Internal'})
+//   .then(employees => {
+//     res.render('internal-all', { employees });
+//   })
+//   .catch(err => console.log(err))
+// });
 
 module.exports = router;
